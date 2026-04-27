f2e32dd2bde62914805501e3bf8cf753e58d2903
pavel levitin <pavlevitin@gmail.com>
2026-04-27 21:45:34 +0300

    fix express wildcard route

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
+app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
